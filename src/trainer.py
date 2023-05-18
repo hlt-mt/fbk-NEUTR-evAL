cc013b431113ca808cf1e7f5ce4e2ea2b637fdcf
Ubuntu <ubuntu@ip-172-30-2-148.eu-west-1.compute.internal>
2023-05-18 14:52:49 +0200

    [!16][LOG] Changes on logging info
    # Why is the change needed?
    
    Log info have some problems:
    - despite set level of logger was set to info, messages were not printed
    - progress bar of tqdm is not much readable when printed in log
    - minor typos
    
    # What changes does the patch introduce?
    
    Improving log messages and setting a handler and a formatter for logger.
    Additionally, info message about model architecture has been added.
    
    # How was this patch tested?
    
    Manual inspection of training logs.

--- src/trainer.py
+++ src/trainer.py
@@ -16,12 +16,13 @@ import logging
 import os
 
 import torch
-from tqdm import tqdm
 
 
 LOGGER = logging.getLogger(__name__)
-log_level = os.environ.get("LOG_LEVEL", "INFO")
-LOGGER.setLevel(getattr(logging, log_level))
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=os.environ.get("LOGLEVEL", "INFO").upper())
 
 
 class BertTrainer:
@@ -47,13 +48,14 @@ class BertTrainer:
         self.save_path = save_path
 
     def train(self):
+        LOGGER.info(f"Model architecture:\n{self.model}")
         total_updates = 0
         for epoch in range(self.num_epochs):
-            LOGGER.info(f"Starting Epoch {epoch}")
+            LOGGER.info(f"Starting Epoch {epoch + 1}")
             self.model.train()
             train_loss = 0
             num_samples = 0
-            for batch in tqdm(self.train_dataloader):
+            for batch in self.train_dataloader:
                 batch = tuple(t.to(self.device) for t in batch)
                 inputs = {"input_ids": batch[0], "attention_mask": batch[1], "labels": batch[2]}
                 self.optimizer.zero_grad()
@@ -66,6 +68,11 @@ class BertTrainer:
                 total_updates += 1
                 train_loss += loss.item()
                 num_samples += inputs["input_ids"].shape[0]
+                if total_updates % 100 == 0:
+                    train_loss_partial = train_loss / num_samples
+                    LOGGER.info(
+                            f"Number Updates: {total_updates}/{len(self.train_dataloader)}, "
+                            f"Training loss: {train_loss_partial:.6f}")
             train_loss /= num_samples
 
             self.model.eval()
@@ -87,10 +94,10 @@ class BertTrainer:
             val_loss /= num_samples
 
             LOGGER.info(
-                f"Epoch {epoch + 1}, "
-                f"NUmber updates: {total_updates}"
-                f"Training loss: {train_loss:.3f}, "
-                f"Validation loss: {val_loss:.3f}")
+                f"Completed Epoch {epoch + 1}: "
+                f"Number updates: {total_updates}, "
+                f"Training loss: {train_loss:.6f}, "
+                f"Validation loss: {val_loss:.6f}")
 
             save_path = os.path.join(self.save_path, f"checkpoint_{epoch + 1}")
             LOGGER.info(f"Saving checkpoint_{epoch + 1}")
